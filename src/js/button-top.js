59e7711da92f1616c2ec4eaad6ae8c608684bf6d
ThisNarciss <tyomaxxx000@gmail.com>
2023-01-30 15:33:51 +0200

    delete button up

--- src/js/button-top.js
+++ src/js/button-top.js
@@ -1,18 +1,18 @@
-const buttonTop = document.querySelector('.button-top');
-const amountScroll = 400;
-window.addEventListener('scroll', onScrollPage);
+// const buttonTop = document.querySelector('.button-top');
+// const amountScroll = 400;
+// window.addEventListener('scroll', onScrollPage);
 
-export default function onScrollPage() {
-  const scrollPage = window.pageYOffset;
-  if (scrollPage > amountScroll) {
-    buttonTop.classList.add('button-top__show');
-    window.setTimeout(() => {
-      this.buttonTop.classList.add('button-top__show');
-    }, 600);
-  } else {
-    buttonTop.classList.remove('button-top__show');
-    window.setTimeout(() => {
-      this.buttonTop.classList.add('button-top__show');
-    }, 600);
-  }
-}
+// export default function onScrollPage() {
+//   const scrollPage = window.pageYOffset;
+//   if (scrollPage > amountScroll) {
+//     buttonTop.classList.add('button-top__show');
+//     window.setTimeout(() => {
+//       this.buttonTop.classList.add('button-top__show');
+//     }, 600);
+//   } else {
+//     buttonTop.classList.remove('button-top__show');
+//     window.setTimeout(() => {
+//       this.buttonTop.classList.add('button-top__show');
+//     }, 600);
+//   }
+// }

ab28cc4bea9ec7c86a41de0b3a16578157766eba
Petro Psekho <psekho.pp@gmail.com>
2023-01-31 11:52:06 +0200

    styled to top button and  fixes poster height

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './js/fetch-movies';
 import './js/search-movies';
 import './js/pagination';
 import './js/film-card-modal';
-import './js/button-top';
+// import './js/button-top';
 import './js/students';
+import './js/to-top-button';
